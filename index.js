136823f56bc726a68d9ce3726b7eedc37188f976
WebDevStu <git@stewart-anderson.co.uk>
2015-12-11 21:24:13 +0000

    Setting up base lib files

--- index.js
+++ index.js
@@ -2,7 +2,18 @@
 
 
 var homeDir = process.env.HOME || process.env.USERPROFILE,
-    fs      = require('fs');
+
+    fs      = require('fs'),
+
+    gitSwap = require('./lib/gitSwap'),
+    gitConfig = require('./lib/gitConfig');
+
+
+
+gitSwap.exists(homeDir + '.gitswap')
+    .then(function () {
+        process.exit(1);
+    });
 
 
 
