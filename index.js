faeb9d12ccd9a519e06af599182e343e27364bd0
WebDevStu <git@stewart-anderson.co.uk>
2015-12-11 22:01:57 +0000

    Changing file management libs

--- index.js
+++ index.js
@@ -4,16 +4,54 @@
 var homeDir = process.env.HOME || process.env.USERPROFILE,
 
     fs      = require('fs'),
+    File    = require('./lib/file'),
 
-    gitSwap = require('./lib/gitSwap'),
-    gitConfig = require('./lib/gitConfig');
+    gitSwap     = new File(homeDir + '/.gitswap'),
+    gitConfig   = new File(homeDir + '/.gitConfig');
 
 
+/**
+ * get the git config
+ */
+gitConfig
+    .exists()
 
-gitSwap.exists(homeDir + '.gitswap')
     .then(function () {
+
+        return gitConfig.read();
+    }, function () {
+
+        console.log('There is .gitconfig file, please save globals first');
         process.exit(1);
+    })
+
+    .then(function (contents) {
+
+        return gitConfig.getUser(contents);
     });
+
+
+/**
+ * get the git swap file
+ */
+gitSwap
+    .exists()
+
+    .then(function () {
+        // exists
+    }, function () {
+        // best create offer creation
+        return gitSwap.create();
+    })
+    .then(function () {
+        // create the file
+        console.log('create the file');
+    }, function () {
+        // meh, they're only bloody happy with it
+    });
+
+
+
 
 
 
